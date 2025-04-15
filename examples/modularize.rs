f3b3859c4858748d395077faaaa65317a2f8a496
Ruediger Klaehn <rklaehn@protonmail.com>
2025-04-15 10:25:45 +0300

    modularize example to figure out what needs to be done

--- examples/modularize.rs
+++ examples/modularize.rs
@@ -1,8 +1,11 @@
+use combined::{CombinedMessage, CombinedProtocol, CombinedService};
 use irpc::{channel::oneshot, Client, LocalSender, Service};
 use irpc_derive::rpc_requests;
 use serde::{Deserialize, Serialize};
 
 mod clock {
+    use std::time::SystemTime;
+
     use super::*;
 
     #[derive(Debug, Clone)]
@@ -19,6 +22,48 @@ mod clock {
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct GetTime;
+
+    pub struct ClockActor {
+        recv: tokio::sync::mpsc::Receiver<ClockMessage>,
+    }
+
+    impl ClockActor {
+        pub fn spawn() -> ClockApi {
+            let (tx, rx) = tokio::sync::mpsc::channel(1);
+            let actor = Self { recv: rx };
+            n0_future::task::spawn(actor.run());
+            let local = LocalSender::<ClockMessage, ClockService>::from(tx);
+            ClockApi {
+                inner: local.into(),
+            }
+        }
+
+        async fn run(mut self) {
+            while let Some(msg) = self.recv.recv().await {
+                self.handle(msg).await;
+            }
+        }
+
+        async fn handle(&mut self, msg: ClockMessage) {
+            match msg {
+                ClockMessage::GetTime(msg) => {
+                    let res = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+                    msg.tx.send(res).await.ok();
+                }
+            }
+        }
+    }
+
+    pub struct ClockApi {
+        inner: Client<ClockMessage, ClockProtocol, ClockService>,
+    }
+
+    impl ClockApi {
+        pub async fn get_time(&self) -> std::result::Result<u64, irpc::Error> {
+            let msg = GetTime;
+            self.inner.rpc(msg).await
+        }
+    }
 }
 
 mod calc {
@@ -43,7 +88,7 @@ mod calc {
         pub b: u128,
     }
 
-    struct CalcActor {
+    pub struct CalcActor {
         recv: tokio::sync::mpsc::Receiver<CalcMessage>,
     }
 
@@ -104,4 +149,25 @@ mod combined {
         Clock(clock::ClockMessage),
         Calc(calc::CalcMessage),
     }
+
+    struct CombinedApi {
+        clock: clock::ClockApi,
+        calc: calc::CalcApi,
+    }
+}
+
+pub struct CombinedApi {
+    client: Client<CombinedMessage, CombinedProtocol, CombinedService>,
+}
+
+#[tokio::main]
+async fn main() {
+    let clock_api = clock::ClockActor::spawn();
+    let calc_api = calc::CalcActor::spawn();
+
+    let time = clock_api.get_time().await.unwrap();
+    println!("Current time: {}", time);
+
+    let result = calc_api.multiply(2, 3).await.unwrap();
+    println!("Multiplication result: {}", result);
 }
