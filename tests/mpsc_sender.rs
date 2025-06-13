3ec5bdd98073a781a0e0c8cac4c525a6b4abdca2
Ruediger Klaehn <rklaehn@protonmail.com>
2025-06-13 11:28:06 +0200

    Add some tests to make sure sender clones behave correctly.

---
+++ tests/mpsc_sender.rs
@@ -0,0 +1,91 @@
+use std::{
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4},
+    time::Duration,
+};
+
+use irpc::{
+    channel::spsc,
+    util::{make_client_endpoint, make_server_endpoint},
+};
+use quinn::Endpoint;
+use testresult::TestResult;
+use tokio::time::timeout;
+
+fn create_connected_endpoints() -> TestResult<(Endpoint, Endpoint, SocketAddr)> {
+    let addr = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0).into();
+    let (server, cert) = make_server_endpoint(addr)?;
+    let client = make_client_endpoint(addr, &[cert.as_slice()])?;
+    let port = server.local_addr()?.port();
+    let server_addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port).into();
+    Ok((server, client, server_addr))
+}
+
+/// Checks that all clones of a `Sender` will get the closed signal as soon as
+/// a send fails with an io error.
+#[tokio::test]
+async fn mpsc_sender_clone_closed_error() -> TestResult<()> {
+    let (server, client, server_addr) = create_connected_endpoints()?;
+    // accept a single bidi stream on a single connection, then immediately stop it
+    let server = tokio::spawn(async move {
+        let conn = server.accept().await.unwrap().await?;
+        let (_, mut recv) = conn.accept_bi().await?;
+        recv.stop(1u8.into())?;
+        TestResult::Ok(())
+    });
+    let conn = client.connect(server_addr, "localhost")?.await?;
+    let (send, _) = conn.open_bi().await?;
+    let send1 = spsc::Sender::<Vec<u8>>::from(send);
+    let send2 = send1.clone();
+    let second_client = tokio::spawn(async move {
+        send2.closed().await;
+    });
+    // send until we get an error because the remote side stopped the stream
+    while send1.send(vec![1, 2, 3]).await.is_ok() {}
+    // check that closed signal was received by the other sender
+    second_client.await?;
+    // server should finish without errors
+    server.await??;
+    Ok(())
+}
+
+/// Checks that all clones of a `Sender` will get the closed signal as soon as
+/// a send future gets dropped before completing.
+#[tokio::test]
+async fn mpsc_sender_clone_drop_error() -> TestResult<()> {
+    let (server, client, server_addr) = create_connected_endpoints()?;
+    // accept a single bidi stream on a single connection, then read indefinitely
+    // until we get an error
+    let server = tokio::spawn(async move {
+        let conn = server.accept().await.unwrap().await?;
+        let (_, mut recv) = conn.accept_bi().await?;
+        let mut buf = vec![0u8; 1024];
+        while recv.read(&mut buf).await.is_ok() {}
+        TestResult::Ok(())
+    });
+    let conn = client.connect(server_addr, "localhost")?.await?;
+    let (send, _) = conn.open_bi().await?;
+    let send1 = spsc::Sender::<Vec<u8>>::from(send);
+    let send2 = send1.clone();
+    let second_client = tokio::spawn(async move {
+        send2.closed().await;
+        // why do I have to do this?
+        // Shouldn't dropping the quinn:SendStream call finish, so the server would get an io error?
+        conn.close(1u8.into(), b"");
+    });
+    // send a lot of data with a tiny timeout, this will cause the send future to be dropped
+    loop {
+        let send_future = send1.send(vec![0u8; 1024 * 1024]);
+        // not sure if there is a better way. I want to poll the future a few times so it has time to
+        // start sending, but don't want to give it enough time to complete.
+        // I don't think now_or_never would work, since it wouldn't have time to start sending
+        if timeout(Duration::from_micros(1), send_future)
+            .await
+            .is_err()
+        {
+            break;
+        }
+    }
+    server.await??;
+    second_client.await?;
+    Ok(())
+}
