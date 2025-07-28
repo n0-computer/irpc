755bec25799fedaca78a45bcbcb813e85ab0697e
Frando <frando@unbiskant.org>
2025-07-28 11:26:27 +0200

    feat: variant feature flag

--- src/util.rs
+++ src/util.rs
@@ -174,7 +174,7 @@ mod quinn_setup_utils {
 #[cfg_attr(quicrpc_docsrs, doc(cfg(feature = "quinn_endpoint_setup")))]
 pub use quinn_setup_utils::*;
 
-#[cfg(feature = "rpc")]
+#[cfg(any(feature = "rpc", feature = "varint-util"))]
 mod varint_util {
     use std::{
         future::Future,
@@ -382,7 +382,8 @@ mod varint_util {
         }
     }
 }
-#[cfg(feature = "rpc")]
+
+#[cfg(any(feature = "rpc", feature = "varint-util"))]
 pub use varint_util::{AsyncReadVarintExt, AsyncWriteVarintExt, WriteVarintExt};
 
 mod fuse_wrapper {
